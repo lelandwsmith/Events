0f0443092efd11a12936ab5603adbdad44c0c038
Bittu Ahlawat <bittuahlawat@gmail.com>
2014-06-30 23:38:58 -0700

    Initial Commit

---
+++ Events/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Events
+//
+//  Created by Ahlawat, Bittu on 6/30/14.
+//  Copyright (c) 2014 eBay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Events/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Events
+//
+//  Created by Ahlawat, Bittu on 6/30/14.
+//  Copyright (c) 2014 eBay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
